80275e088cb2c4b8164be777e9b18a7695ef1d90
Shonit Sahoo <shonit.saahil@gmail.com>
2023-08-28 04:24:22 +0530

    configured environment variables and mongoose

--- app.js
+++ app.js
@@ -1,47 +1,116 @@
-const express=require('express');
-const morgan=require('morgan');
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
 
-const tourRouter=require('./routes/tourRoutes');
-const userRouter=require('./routes/userRoutes')
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
 
-const app=express();
+// Start express app
+const app = express();
 
-//  1)MIDDLEWARES
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'));
+app.enable('trust proxy');
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-// app.use(morgan('tiny'));
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
-app.use((req,res,next)=>{
-    console.log('Hello from the middleware');
-    next();
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+app.use(compression());
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
 });
 
-app.use((req,res,next)=>{
-    req.requestTime=new Date().toISOString();
-    next();
+// 3) ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-// app.get('/',(req,res)=>{
-//     res
-//         .status(200)
-//         .json({message:'Hello from the server side',app:'Natours'});
-// });
+app.use(globalErrorHandler);
 
-// app.post('/',(req,res)=>{
-//     res.send('You can post to that endpoint...');
-// });
-
-// app.get('/api/v1/tours',getAllTours);
-// app.get('/api/v1/tours/:id',getTour);
-// app.post('/api/v1/tours',createTour);
-// app.patch('/api/v1/tours/:id',updateTour);
-// app.delete('/api/v1/tours/:id',deleteTour);
-
-// 2) ROUTES
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
-
-module.exports=app;
+module.exports = app;
